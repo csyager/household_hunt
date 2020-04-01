be38022b33c722ddd3ac6bb771bf7fc1cfa5d6f4
Connor <csy7ay@virginia.edu>
2020-04-01 17:17:05 -0400

    UI for landing page complete

---
+++ src/register.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Button, FormControl } from 'react-bootstrap';
+import './App.css';
+
+class RegisterForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: ''};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmit(event) {
+        alert("Name: " + this.state.value);
+    }
+
+    render() {
+        return(
+            <form action="" method="GET">
+                <label>
+                    Enter your name to play:<br />
+                    <FormControl type="text" name="name" value={this.state.value} onChange={this.handleChange} placeholder="Name" />
+                </label><br />
+                <Button variant="primary" type="submit">Start a Game</Button>&nbsp;&nbsp;
+                <Button variant="secondary" type="submit">Join a Game</Button>
+            </form>
+        );
+    }
+}
+export default RegisterForm
